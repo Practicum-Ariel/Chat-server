306f3e795dfd6613e937be13efec10e280a28bbc
AviadDerli <aviad@workin.co.il>
2024-06-19 12:17:27 +0300

    data + api

---
+++ data.js
@@ -0,0 +1,11 @@
+module.exports = {
+    users : {},
+    rooms : ['room 1','room 2','room 3']
+}
+
+
+// {
+//     Socket.id : "Moshe123!",
+//     Socket.id : "AviAvi8"
+// }
+
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express'),
     PORT = 2500,
     cors = require('cors');
 
+app.use(cors())
+app.use(express.json())
+
 const http = require('http')
 const server = http.createServer(app)
 
@@ -13,20 +16,36 @@ const io = new socketIO.Server(server, {
     }
 })
 
-io.on('connection', (socket) => {
-    console.log("connecting to server:",socket.id);
-    
-    socket.emit('welcome',socket.id)
+const { users, rooms } = require('./data')
 
-    socket.on('new-msg-client',(data)=>{
-        console.log(socket.id, " | new message: ",data);
-        io.emit('msg-server',data);
+io.on('connection', (socket) => {
+    console.log("connecting to server:", socket.id);
+
+    let userName = socket.handshake.query?.userName
+    if (userName) users[socket.id] = userName
+
+    socket.on('join', (userName) => {
+        users[socket.id] = userName
+    })
+
+    socket.emit('welcome', socket.id)
+
+    socket.on('new-msg-client', (data) => {
+        console.log(socket.id, " | new message: ", data);
+        io.emit('msg-server', data);
     })
 })
 
 
-app.get('/', async (req, res) => {
-    res.send('api work!')
+app.get('/room', async (req, res) => {
+    res.send(rooms)
+})
+app.get('/room/:roomId', async (req, res) => {
+    res.send(rooms.includes)
+})
+app.post('/login', async (req, res) => {
+    let isExist = Boolean(req.body.userName) && Object.values(users).includes(req.body.userName)
+    res.send(isExist)
 })
 
 server.listen(PORT, () => console.log("##### Server is UP #####"))
